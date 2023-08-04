682552e2265e9c12a02cafcec0359852477f1abd
winniemei <wmei91@gmail.com>
2023-08-04 14:03:16 -0400

    added three circles of color

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 // Write your Color component here
+const Color = (props) => {
+  return <div className={props.color}> </div>
+}
 
 const App = () => {
   return (
@@ -7,7 +10,11 @@ const App = () => {
         <div>Currently selected: </div>
         <div className="red">red</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color="red"/>
+        <Color color="blue"/>
+        <Color color="green"/>
+      </div>
     </div>
   );
 };
